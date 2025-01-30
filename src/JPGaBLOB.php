d3b08fb24ad1a97ff684a745d3cb1fa1313dd1d3
ApartadoContacto <miguel.s343638@cesurformacion.com>
2025-01-30 12:08:34 +0100

    falta la imagen en binario y la pasarela , por lo demás bien

--- src/JPGaBLOB.php
+++ src/JPGaBLOB.php
@@ -9,16 +9,39 @@
 //echo "el tamaño es $TamanioImagen"
 
 
-$ruta="C:\Users\trujo\Pictures\AN2.jpg";
+//$ruta="C:\Users\trujo\Pictures\AN2.jpg";
 // la imagen q queramos subir debe estar en .jpg
 //función para el tamaño de la imagen
-$TamanioImagen=$_FILES['imagen']['size'];
+//$TamanioImagen=$_FILES['imagen']['size'];
 
-$LaImagen=fopen("C:\Users\trujo\Pictures\AN2.jpg","r");
+//$LaImagen=fopen("C:\Users\trujo\Pictures\AN2.jpg","r");
 //Donde pone tamaño iría una función
-$BitsArchivo=fread($LaImagen, $TamanioImagen);
-fclose($LaImagen);
+//$BitsArchivo=fread($LaImagen, $TamanioImagen);
+//fclose($LaImagen);
 
-echo $BitsArchivo;
-echo "el tamaño es $TamanioImagen";
+//echo $BitsArchivo;
+//echo "el tamaño es $TamanioImagen";
+//
+$sql = "select link from tabla";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+    while($row = $result->fetch_assoc()) {
+        echo "<img src='".$row["link"]."' >";
+    }
+}
+$conn->close();
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
