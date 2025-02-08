d7fd7a51c7410e67bc9826e25371c3f05aab8db5
Letder40 <adrian04rv@gmail.com>
2025-02-08 03:44:50 +0100

    fix: conn_root fue removido en el merge anterior

--- src/includes/connect.php
+++ src/includes/connect.php
@@ -5,8 +5,5 @@ $mysqli_password = "ArchTheBest";
 $mysqli_database = "alamedamotors";
 
 $conn = new mysqli($mysqli_servername, $mysqli_username, $mysqli_password, $mysqli_database);
-
-if ($conn->connect_error) {
-    die("Error en la conexión: " . $conn->connect_error);
-}
+$conn_root = new mysqli($mysqli_servername, "root", $mysqli_password, $mysqli_database);
 ?>
