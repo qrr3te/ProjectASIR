d2cbf9cb29c39e29266d1e6838382b83f301311f
Letder40 <adrian04rv@gmail.com>
2025-02-17 06:15:50 +0100

    Rust server_stats: Datos de memoria ram con /proc/meminfo, creado listener en el puerto 14500/tcp devuelve json con estadisticas del servidor al conectarse

--- server_stats/src/main.rs
+++ server_stats/src/main.rs
@@ -1,6 +1,10 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
+use std::net::TcpListener;
 use std::{error::Error, thread, time::Duration};
 use mysql::*;
 use mysql::prelude::*;
+use serde::Serialize;
 
 struct MysqlConfig {
     host: Option<String>,
@@ -12,10 +16,7 @@ struct MysqlConfig {
 impl MysqlConfig {
     fn default() -> Self {
         Self { 
-            #[cfg(target_os = "linux")]
             host: Some("172.20.0.11".to_owned()),
-            #[cfg(target_os = "windows")]
-            host: Some("127.0.0.1".to_owned()),
             username: Some("root".to_owned()),
             password: Some("ArchTheBest".to_owned()),
             database: Some("alamedamotors".to_owned())
@@ -23,6 +24,7 @@ impl MysqlConfig {
     } 
 }
 
+#[derive(Serialize)]
 struct DatabaseSize {
     name: String,
     size: f32,
@@ -34,14 +36,36 @@ impl DatabaseSize {
     }
 }
 
+#[derive(Serialize)]
 #[repr(C)]
 struct DiskSize {
     total: i32,
     available: i32,
 }
 
+#[derive(Serialize)]
+struct Memory {
+    total: i32,
+    available: i32,
+}
+
+#[derive(Serialize)]
+struct ServerStats {
+    database: Vec<DatabaseSize>,
+    disk: DiskSize,
+    memory: Memory,
+}
+
 extern "C" {
     fn get_disksize() -> DiskSize;
+}
+
+fn get_next_value(bufreader: &mut BufReader<File>) -> i32 {
+    let mut buf = String::new();
+    bufreader.read_line(&mut buf).unwrap();
+    let mut buf_splitted: Vec<&str> = buf.split(" ").collect();
+    buf_splitted.pop(); 
+    return buf_splitted.into_iter().last().unwrap().parse().unwrap();
 }
 
 fn try_connect() -> Conn {
@@ -63,7 +87,7 @@ fn try_connect() -> Conn {
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn get_server_stats() -> Result<String, Box<dyn Error>> {
     let mut conn = try_connect();
     let mut results: Vec<DatabaseSize> = Vec::with_capacity(3);
     let rows: Vec<Row> = conn.exec("SELECT table_schema 'database', ROUND(SUM(data_length + index_length) / 1024 / 1024, 1) FROM information_schema.tables WHERE table_schema IN ('alamedamotors', 'mysql', 'information_schema') GROUP BY table_schema;", ())?;
@@ -72,17 +96,35 @@ fn main() -> Result<(), Box<dyn Error>> {
         results.push(result);
     }
 
-    for result in results {
-        println!("{} -> {}", result.name, result.size)
-    }
-
     let disk_size: DiskSize;
     unsafe {
         disk_size = get_disksize();
     }
+    
+    let meminfo_file = File::open("/proc/meminfo")?;
+    let mut bufreader = BufReader::new(meminfo_file);
 
-    println!("{}GB", disk_size.total);
-    println!("{}GB", disk_size.available);
+    let mem_stats = Memory {
+        total: get_next_value(&mut bufreader),
+        available: get_next_value(&mut bufreader) 
+    };
 
+    let server_stats = ServerStats {
+        database: results,
+        disk: disk_size,
+        memory: mem_stats,
+    };
+    
+    Ok(serde_json::to_string(&server_stats)?)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind("172.20.0.1:14500")?;
+
+    for stream in listener.incoming() {
+        let mut stream = stream?;
+        let server_stats = get_server_stats()?;
+        stream.write(&server_stats.into_bytes())?;
+    }
     Ok(())
 }
